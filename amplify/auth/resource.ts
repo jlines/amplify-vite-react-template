731c6af3de2bc924d48476decc53aa2cc04f84db
Jason Lines <jlines@solidstatenetworks.com>
2025-10-21 15:02:34 -0400

    update auth menthod

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,9 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { referenceAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+export const auth = referenceAuth({
+  userPoolId: 'us-east-1_ht2H5Bj34',
+  identityPoolId: 'us-east-1:3b5f4e77-0ee4-4d39-8225-798607deb37d',
+  authRoleArn: 'arn:aws:iam::xxxx:role/amplify-xxxx-mai-amplifyAuthauthenticatedU-xxxx',
+  unauthRoleArn: 'arn:aws:iam::xxxx:role/amplify-xxxx-mai-amplifyAuthunauthenticate-xxxx',
+  userPoolClientId: '2jtea7nu27a5qps9t1e04ld2g8',
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import '@aws-amplify/ui-react/styles.css';
+
+outputs.Auth = {
+
+    Cognito: {
+      userPoolId: "us-east-1_ht2H5Bj34",
+      userPoolClientId: "2jtea7nu27a5qps9t1e04ld2g8",
+      identityPoolId: "us-east-1:3b5f4e77-0ee4-4d39-8225-798607deb37d",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  }
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator>
+      <App />
+    </Authenticator>
   </React.StrictMode>
 );
